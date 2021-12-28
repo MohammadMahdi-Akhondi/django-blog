6c8900ba1e56040cf6a6829b5839df364149f1bb
MohammadMahdi-Akhondi <mohammad.a8098@gmail.com>
2021-12-29 02:28:36 +0330

    Create a model and register it

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Article(models.Model):
+    STATUS_CHOICES = (
+        ('d', 'draft'),
+        ('p', 'published')
+    )
+    title       = models.CharField(max_length=100)
+    slug        = models.SlugField(max_length=50, unique=True)
+    description = models.TextField()
+    thumbnail   = models.ImageField(upload_to="images")
+    publish     = models.DateTimeField(default=timezone.now)
+    created     = models.DateTimeField(auto_now_add=True)
+    updated     = models.DateTimeField(auto_now=True)
+    status      = models.CharField(max_length=1, choices=STATUS_CHOICES)
