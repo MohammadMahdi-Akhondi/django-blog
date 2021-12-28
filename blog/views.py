ed31832ee6a1039ded2beedce18c6fd92e95f428
MohammadMahdi-Akhondi <mohammad.a8098@gmail.com>
2021-12-29 01:49:04 +0330

    Start templates

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello Django!")
+    return render(request, "blog/home.html")
