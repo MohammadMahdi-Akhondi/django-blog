41706f84a96140da7ec14b4ae80918f7de9bda5d
MohammadMahdi-Akhondi <mohammad.a8098@gmail.com>
2022-01-01 19:48:10 +0330

    Creating view and template of detail and update base.html & index.html

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
 
 def home(request):
@@ -6,3 +6,10 @@ def home(request):
         "articles" : Article.objects.filter(status = "p").order_by("-publish")
     }
     return render(request, "blog/index.html", context)
+
+def detail(request, slug):
+    context = {
+        "articles" : Article.objects.filter(status = "p").order_by("-publish")[:3],
+        "article" : get_object_or_404(Article, slug = slug, status = "p")
+    }
+    return render(request, "blog/detail.html", context)
