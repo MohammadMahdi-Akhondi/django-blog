640e28536f1fdd70243056a8c77b18a8f59a1d92
MohammadMahdi-Akhondi <mohammad.a8098@gmail.com>
2022-01-03 23:42:39 +0330

    Add persian_numbers_converter function

--- extensions/utils.py
+++ extensions/utils.py
@@ -1,6 +1,25 @@
 from . import jalali
 from django.utils import timezone
 
+def persian_numbers_converter(mystr):
+    numbers = {
+        "0":"۰",
+        "1":"۱",
+        "2":"۲",
+        "3":"۳",
+        "4":"۴",
+        "5":"۵",
+        "6":"۶",
+        "7":"۷",
+        "8":"۸",
+        "9":"۹",
+    }
+
+    for e, p in numbers.items():
+        mystr = mystr.replace(e, p)
+
+    return mystr
+
 def jalali_converter(time):
     time = timezone.localtime(time)
 
@@ -16,4 +35,4 @@ def jalali_converter(time):
             break
 
     output = f"{time_to_list[2]} {time_to_list[1]} {time_to_list[0]}, ساعت {time.hour}:{time.minute}"   
-    return output
+    return persian_numbers_converter(output)
