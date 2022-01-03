1ed07ed95243dc609d294c3586ede928b72f7d0a
MohammadMahdi-Akhondi <mohammad.a8098@gmail.com>
2022-01-04 01:41:10 +0330

    Show the category model in the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -12,10 +12,14 @@ admin.site.register(Category, CategoryAdmin)
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'jpublish', 'status')
+    list_display = ('title', 'slug', 'jpublish', 'status', 'category_to_str')
     list_filter = ('publish', 'status')
     search_fields = ('title', 'description')
     prepopulated_fields = {'slug' : ('title',)}
     ordering = ['-status', '-publish']
+
+    def category_to_str(self, obj):
+        return "و ".join([category.title for category in obj.category.all()])
+    category_to_str.short_description = "دسته بندی"
 
 admin.site.register(Article, ArticleAdmin)
