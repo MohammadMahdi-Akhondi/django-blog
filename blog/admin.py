36c960cbadb044c984bbfa2cddb5637a56991b4a
MohammadMahdi-Akhondi <mohammad.a8098@gmail.com>
2022-01-05 01:42:00 +0330

    Persianization and right alignment

--- blog/admin.py
+++ blog/admin.py
@@ -19,7 +19,7 @@ class ArticleAdmin(admin.ModelAdmin):
     ordering = ['-status', '-publish']
 
     def category_to_str(self, obj):
-        return "و ".join([category.title for category in obj.category.all()])
+        return ", ".join([category.title for category in obj.category.all()])
     category_to_str.short_description = "دسته بندی"
 
 admin.site.register(Article, ArticleAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Article
+from .models import Article, Category
 
 def home(request):
     context = {
-        "articles" : Article.objects.filter(status = "p").order_by("-publish")
+        "articles" : Article.objects.filter(status = "p").order_by("-publish"),
+        "categories" : Category.objects.filter(status = True)
     }
     return render(request, "blog/index.html", context)
 
 def detail(request, slug):
     context = {
         "articles" : Article.objects.filter(status = "p").order_by("-publish")[:3],
-        "article" : get_object_or_404(Article, slug = slug, status = "p")
+        "article" : get_object_or_404(Article, slug = slug, status = "p"),
+        "categories" : Category.objects.filter(status = True)
     }
     return render(request, "blog/detail.html", context)
 
 def blog(request):
     context = {
-        "articles" : Article.objects.filter(status = "p").order_by("-publish")
+        "articles" : Article.objects.filter(status = "p").order_by("-publish"),
+        "categories" : Category.objects.filter(status = True)
     }
     return render(request, "blog/blog.html", context)
