1cc8adfb03298eb4388ed571d844135176f2b04f
MohammadMahdi-Akhondi <mohammad.a8098@gmail.com>
2022-01-07 19:58:27 +0330

    pagination of blog view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Article, Category
 
 def home(request):
@@ -14,8 +15,12 @@ def detail(request, slug):
     return render(request, "blog/detail.html", context)
 
 def blog(request):
+    article_list = Article.objects.published().order_by("-publish")
+    paginator = Paginator(article_list, 6)
+    page_number = request.GET.get('page')
+    articles = paginator.get_page(page_number)
     context = {
-        "articles" : Article.objects.published().order_by("-publish")
+        "articles" : articles
     }
     return render(request, "blog/blog.html", context)
 
