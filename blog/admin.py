2863bd8729fce4d48b1fb717afbba218019c3567
MohammadMahdi-Akhondi <mohammad.a8098@gmail.com>
2022-01-10 10:07:21 +0330

    Create multiple actions in the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -6,6 +6,25 @@ class CategoryAdmin(admin.ModelAdmin):
     list_filter = (['status'])
     search_fields = ('title', 'slug')
     prepopulated_fields = {'slug' : ('title',)}
+    actions = ['make_publish', 'make_draft']
+
+    def make_publish(self, request, queryset):
+        rows_updated = queryset.update(status = True)
+        if rows_updated == 1:
+            message_bit = '.با موفقیت نمایش داده شد'
+        else:
+            message_bit = '.با موفقیت نمایش داده شدند'
+        self.message_user(request, "{} دسته بندی {}".format(rows_updated, message_bit))
+    make_publish.short_description = "نشان دادن دسته بندی های انتخاب شده"
+
+    def make_draft(self, request, queryset):
+        rows_updated = queryset.update(status = False)
+        if rows_updated == 1:
+            message_bit = '.با موفقیت مخفی شد'
+        else:
+            message_bit = '.با موفقیت مخفی شدند'
+        self.message_user(request, "{} مقاله {}".format(rows_updated, message_bit))
+    make_draft.short_description = "مخفی کردن دسته بندی های انتخاب شده"
 
 admin.site.register(Category, CategoryAdmin)
 
@@ -17,6 +36,25 @@ class ArticleAdmin(admin.ModelAdmin):
     search_fields = ('title', 'description')
     prepopulated_fields = {'slug' : ('title',)}
     ordering = ['-status', '-publish']
+    actions = ['make_publish', 'make_draft']
+
+    def make_publish(self, request, queryset):
+        rows_updated = queryset.update(status = 'p')
+        if rows_updated == 1:
+            message_bit = '.با موفقیت منتشر شد'
+        else:
+            message_bit = '.با موفقیت منتشر شدند'
+        self.message_user(request, "{} مقاله {}".format(rows_updated, message_bit))
+    make_publish.short_description = "منتشر کردن مقالات انتخاب شده"
+
+    def make_draft(self, request, queryset):
+        rows_updated = queryset.update(status = 'd')
+        if rows_updated == 1:
+            message_bit = '.با موفقیت پیش‌نویش شد'
+        else:
+            message_bit = '.با موفقیت پیش‌نویس شدند'
+        self.message_user(request, "{} مقاله {}".format(rows_updated, message_bit))
+    make_draft.short_description = "پیش‌نویس کردن مقالات انتخاب شده"
 
     def category_to_str(self, obj):
         return ", ".join([category.title for category in obj.published_categories()])
