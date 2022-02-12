bf739fbb20804a28835a647829fe8b1268e8b0f5
MohammadMahdi-Akhondi <mohammad.a8098@gmail.com>
2022-02-12 18:55:51 +0330

    Add the most visited articles section

--- blog/templatetags/base_tags.py
+++ blog/templatetags/base_tags.py
@@ -1,4 +1,7 @@
 from django import template
+from django.db.models import Count
+from datetime import datetime, timedelta
+
 from ..models import Category, Article
 
 register = template.Library()
@@ -8,4 +11,11 @@ def sidebar():
     return {
         "articles" : Article.objects.filter(status = "p").order_by("-publish")[:3],
         "categories" : Category.objects.filter(status = True)
+    }
+
+@register.inclusion_tag('blog/partials/popular_articles.html')
+def popular_articles():
+    last_month = datetime.today() - timedelta(days = 30)
+    return {
+        "popular_articles" : Article.objects.published().filter(articlehit__created__gte = last_month).annotate(count = Count('hits')).order_by('-count', '-publish')
     }
